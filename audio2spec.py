781be2f945e3db40df9a2bb83c9bc0c4b89d2d98
Andrew Ferg <uberferg@gmail.com>
2016-05-09 15:56:47 -0400

    created spectrogram scripts

--- audio2spec.py
+++ audio2spec.py
@@ -4,13 +4,14 @@
 #
 ###############################################################################
 
-from lib.decode import audio2wav
+import numpy as np
+from matplotlib import pyplot as plt
 
-def all_au_to_wav():
-    in_directory = "genres/"
-    out_directory = "genres_wav/"
-    genres = ["blues",
-              "classical",
+from lib.decode import audio2wav
+from lib.spectrogram import savespec
+
+genres = ["blues",
+          "classical",
 	      "country",
 	      "disco",
 	      "hiphop",
@@ -19,6 +20,10 @@ def all_au_to_wav():
 	      "pop",
 	      "reggae",
 	      "rock"]
+
+def all_au_to_wav():
+    in_directory = "genres/"
+    out_directory = "genres_wav/"
 
     for genre in genres:
         print('\nBegin directory: "' + in_directory + '/' + genre + ".")
@@ -36,6 +41,27 @@ def all_au_to_wav():
             audio2wav(infile, outfile)
         print('Finished directory.')
 
-if __name__ == "__main__":
-    all_au_to_wav()
+def all_wav_to_spec():
+    in_directory = "genres_wav/"
+    out_directory = "spectrograms/"
 
+    for genre in genres:
+        print('\nBegin directory: "' + in_directory + '/' + genre + ".")
+        for i in range(100):
+            if i < 10:
+                j = "0" + str(i)
+            else:
+                j = str(i)
+            infile = in_directory \
+                     + genre + "/" \
+                     + genre + ".000" + j + ".wav"
+            outfile = out_directory \
+                     + genre + "/" \
+                     + genre + ".000" + j + ".spec.png"
+            savespec(infile, outfile)
+        print('Finished directory.')
+
+
+if __name__ == "__main__":
+    #all_au_to_wav()
+    all_wav_to_spec()
